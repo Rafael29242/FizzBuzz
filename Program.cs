4ad5bf2be85abd2b98c66f38def5bdfc9404ed62
Rafi Gomez <77979382+Rafael29242@users.noreply.github.com>
2021-04-07 18:40:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ch5_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int f = 1; f <= 100; f++)
+
+            {
+
+                if (f % 3 == 0 && f % 5 == 0)
+
+                {
+
+                    Console.WriteLine($"{f} FizzBuzz");
+
+                }
+
+                else if (f % 3 == 0)
+
+                {
+
+                    Console.WriteLine($"{f} Fizz");
+
+                }
+
+                else if (f % 5 == 0)
+
+                {
+
+                    Console.WriteLine($"{f} Buzz");
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine(f);
+
+                }
+
+            }  
+
+        }
+    }
+}
